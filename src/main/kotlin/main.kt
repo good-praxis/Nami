adfc87119e9605ab83d2637aefb7374351972360
Mavial <santiago.martinez@code.berlin>
2019-03-07 13:45:12 +0100

    floobits version

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+package com.code.gamerg8.nami
+
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWVulkan.*
+import org.lwjgl.vulkan.*
+
+
+
+fun main(args: Array<String>) {
+    println("hello")
+}
